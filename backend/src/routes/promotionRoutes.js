e309f59ec0100399f341eb5d3f6f600caad5fc48
thihasM <thihas2004@gmail.com>
2026-04-13 18:19:42 +0530

    backend error fix for promotions

--- backend/src/routes/promotionRoutes.js
+++ backend/src/routes/promotionRoutes.js
@@ -10,30 +10,30 @@ const {
   ratePromotion,
   getRetailerPromotions
 } = require('../controllers/promotionController');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 
 // Promotion Manager: create promotion
-router.post('/', authenticateToken, createPromotion);
+router.post('/', protect, createPromotion);
 
 // All users: view all active promotions
-router.get('/', authenticateToken, getAllPromotions);
+router.get('/', protect, getAllPromotions);
 
 // Get single promotion details
-router.get('/:id', authenticateToken, getPromotionById);
+router.get('/:id', protect, getPromotionById);
 
 // Promotion Manager: update promotion
-router.put('/:id', authenticateToken, updatePromotion);
+router.put('/:id', protect, updatePromotion);
 
 // Retailer: opt-in to promotion
-router.post('/:id/opt-in', authenticateToken, retailerOptInPromotion);
+router.post('/:id/opt-in', protect, retailerOptInPromotion);
 
 // Promotion Manager: assign distributor to retailer for a promotion
-router.post('/:id/assign-distributor', authenticateToken, assignDistributorToRetailer);
+router.post('/:id/assign-distributor', protect, assignDistributorToRetailer);
 
 // Retailer: rate promotion
-router.post('/:id/rate', authenticateToken, ratePromotion);
+router.post('/:id/rate', protect, ratePromotion);
 
 // Retailer: get their opted-in promotions
-router.get('/retailer/my-promotions', authenticateToken, getRetailerPromotions);
+router.get('/retailer/my-promotions', protect, getRetailerPromotions);
 
 module.exports = router;
